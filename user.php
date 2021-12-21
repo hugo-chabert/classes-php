ee95f9b03e3b94b5b5688886d66cb4a3b110f476
Hugo <hugo.chabert@laplateforme.io>
2021-12-21 15:44:53 +0100

    C'est nul ca marchez pas

--- user.php
+++ user.php
@@ -7,7 +7,7 @@ class User{
     public $firstname;
     public $lastname;
 
-    public function __construct(){
+    public function __construct($id,$login,$email,$firstname,$lastname){
         $this->id = $id;
         $this->login = $login;
         $this->email = $email;
@@ -19,13 +19,21 @@ class User{
         $bdd = mysqli_connect('localhost', 'root', 'root', 'classes');
         mysqli_set_charset($bdd,'utf8');
         $requete = mysqli_query($bdd, "INSERT INTO utilisateurs (login, password, email, firstname, lastname) VALUES ('$login','$password','$email','$firstname','$lastname')");
-    }
-
-    public function connect($login,$password){
-        
+        return '<tr><td>'.$login.'</td><td>'.$password.'</td><td>'.$email.'</td><td>'.$firstname.'</td><td>'.$lastname.'</td></tr>';
     }
 }
 
-
+$datas = new User(1, $_POST('login'), $_POST('email'), $_POST('firstname'), $_POST('lastname'));
+$datas->register();
+var_dump($datas);
 
 ?>
+
+<form action="" method="POST">
+    <input type="text" name="login" placeholder="Login"></br>
+    <input type="text" name="password" placeholder="Mot de Passe"></br>
+    <input type="text" name="email" placeholder="Email"></br>
+    <input type="text" name="firstname" placeholder="Prenom"></br>
+    <input type="text" name="lastname" placeholder="Nom"></br>
+    <button type="submit" name="send">Envoyer</button>
+</form>
